47fff354a056814e3134840978b1d307b5dbb027
Ashish-1016 <ajeswani80@gmail.com>
2023-07-31 18:14:11 +0530

    first Edfling commit

---
+++ src/Pages/Page1.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SideBarCourse from "../Components/SidebarCourses";
+
+import EventBanner from "../Components/EventBanner";
+import ContactUs from "../Components/ContactUs";
+
+const Page1 = () => {
+  return (
+    <main className="flex justify-center items-center m-12">
+      <ContactUs />
+    </main>
+  );
+};
+
+export default Page1;
